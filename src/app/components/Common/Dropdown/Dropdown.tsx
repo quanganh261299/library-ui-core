f5c501498592515fed4c14e1524bdc5fe8c0e94c
quanganh <quanganhdoan5599@gmail.com>
2024-08-24 09:00:29 +0700

    fix/fix-bug-not-found-modules

---
+++ src/app/components/Common/Dropdown/Dropdown.tsx
@@ -0,0 +1,7 @@
+const Example = () => {
+  return (
+    <>Example</>
+  )
+}
+
+export default Example
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './global.scss';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { route } from './routes/route';
